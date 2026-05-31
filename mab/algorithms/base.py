44bc0ca976338b919cf3863bf5813462460ae273
kishan-chakraborty <kishanchakraborty833@gmail.com>
2026-05-31 16:50:29 +0530

    folder restructuring

---
+++ mab/algorithms/base.py
@@ -0,0 +1,21 @@
+class BasePolicy:
+    """Base class for all MAB algorithms."""
+
+    name = "base_policy"
+
+    def __init__(self, n_arms, seed=None, **kwargs):
+        self.n_arms = n_arms
+        self.n_iters = 0
+
+    def select_arm(self):
+        """Select an arm using UCB rule."""
+        raise NotImplementedError("This method must be implemented by subclasses.")
+
+    def update(self, arm, reward):
+        """Update the empirical mean of chosen arm."""
+        self.n_iters += 1
+        raise NotImplementedError("This method must be implemented by subclasses.")
+
+    def reset(self):
+        """Reset the policy to the initial state."""
+        self.n_iters = 0
