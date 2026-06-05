b7610b4633a256e03a5dae3bed7341dcf69ee850
Kishan <kishanchakraborty833@gmail.com>
2026-06-05 20:24:10 +0530

    no. of markers in a plot (chnaged)

--- mab/environment/plots.py
+++ mab/environment/plots.py
@@ -1,17 +1,19 @@
 import matplotlib.pyplot as plt
 
 
-def plot_regret(regret_dict, marker_spacing=20):
+def plot_regret(regret_dict, n_markers=20):
     """
     Plot the cumulative regret for different algorithms.
     Parameters:
         regret_dict (dict): algo_name -> cumulative_regret
+        n_markers (int): Number of markers to show on the plot for better visibility.
     """
     plt.figure(figsize=(10, 6))
 
     # Plot marker at regular intervals
     marker_style = ["o", "*", "^", "s", "D"]
     for i, (algo, regret) in enumerate(regret_dict.items()):
+        marker_spacing = len(regret) // n_markers
         plt.plot(regret, label=algo, marker=marker_style[i % len(marker_style)], markevery=marker_spacing)
         
     plt.xlabel("Time Steps")
